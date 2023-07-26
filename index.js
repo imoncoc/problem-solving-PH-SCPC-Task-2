795a609998588ee8be99fe7f9c24bae2832b90c7
Md Imon Hossain <imoncoc0@gmail.com>
2023-07-26 09:48:45 +0600

    Solved Task 1

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 /*
 // Task 1 : Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method. 
 
-const str = "hello world";
-let ans = "";
+function reverseString(str){
+    let ans = "";
 
-for(let i = str.length - 1; i >= 0; i--){
-    ans += str[i];
+    for (let i = str.length - 1; i >= 0; i--) {
+      ans += str[i];
+    }
+    return ans;
 }
-console.log(ans);
+
+const str = "hello world";
+
+console.log(reverseString(str));
 */
 
 /*
